660c61147a41334be1380c9431a5aa40479448a2
Adnan <adnanmia.cse@gmail.com>
2026-06-08 10:25:40 +0600

    pushh

--- backend/main.py
+++ backend/main.py
@@ -711,12 +711,9 @@ if FRONTEND_DIR:
             raise HTTPException(404, "icons.svg not found")
         return FileResponse(path, media_type="image/svg+xml")
 
-    @app.get("/favicon.png")
-async def favicon_png():
-    path = os.path.join(public_dir, "favicon.png")
-    if not os.path.isfile(path):
-        raise HTTPException(404, "favicon.png not found")
-    return FileResponse(path, media_type="image/png")
+    @app.get("/favicon.svg")
+    async def favicon_svg():
+        path = os.path.join(public_dir, "favicon.svg")
         if not os.path.isfile(path):
             raise HTTPException(404, "favicon.svg not found")
         return FileResponse(path, media_type="image/svg+xml")
